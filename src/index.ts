db4b30712e45d7cb2e0d74833763616f6258a2e9
Nikita Chkhaidze <nikita.chkhaidze@gmail.com>
2024-06-20 20:44:12 +0400

    added mongo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import {productsRoute} from "../routes/products";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-const apiUrl = process.env.API_URL;
+const apiUrl = process.env.API_URL ?? '';
+const mongoUrl = process.env.MONGO_URL ?? '';
+
+mongoose.connect(mongoUrl)
+    .then(() => console.log('DB connection successful'))
+    .catch(console.log);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
