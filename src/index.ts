4873a28e197f7a0afa41f838261d22df77970883
Nikita Chkhaidze <nikita.chkhaidze@gmail.com>
2024-07-02 20:10:33 +0400

    added CORS whitelist

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import productsRoute from "./routes/products";
 import authRoute from './routes/auth';
 import userRoute from './routes/user';
+import cors, {CorsOptions} from 'cors';
 
 dotenv.config();
 
@@ -11,12 +12,17 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 const apiUrl = process.env.API_URL ?? '';
 const mongoUrl = process.env.MONGO_URL ?? '';
+const corsOptions: CorsOptions = {
+  origin: process.env.CORS_WHITELIST ?? ''
+}
+
 
 mongoose.connect(mongoUrl)
     .then(() => console.log('DB connection successful'))
     .catch(console.log);
 
 app.use(express.json());
+app.use(cors(corsOptions))
 app.use(`${apiUrl}/products/`, productsRoute);
 app.use(`${apiUrl}/auth/`, authRoute);
 app.use(`${apiUrl}/user/`, userRoute);
